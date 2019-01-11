ac11f01977257f9161021071c26f57f497111a10
DenisShulyak <46023442+DenisShulyak@users.noreply.github.com>
2019-01-11 17:58:47 +0600

    Update AutoIrrigationSystem.cs

--- Work/AutoIrrigationSystem.cs
+++ Work/AutoIrrigationSystem.cs
@@ -9,20 +9,10 @@ namespace Work
     class AutoIrrigationSystem
     {
         private Field[] _fields; // поля для орошения
-        private IrrigationType _irrigationType; // тип орошения
 
         public Field[] GetField()
         {
             return _fields;
-        }
-
-        public IrrigationType GetIrrigationType()
-        {
-            return _irrigationType;
-        }
-        public void SetIrrigationType(IrrigationType irrigationType)
-        {
-            _irrigationType = irrigationType;
         }
     }
 }
